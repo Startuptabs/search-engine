a2c61e61bdb8b5d8b4e8b24b67427dce71c96368
James Lee <uptownhr@gmail.com>
2015-12-14 18:44:03 -0800

    added a spider method to run in loop

--- models/Site.js
+++ models/Site.js
@@ -4,19 +4,29 @@ const mongoose = require('mongoose'),
 
 
 const Path = new Schema({
-  path: {type: String, unique: true},
+  path: {type: String},
   title: {type: String},
   description: {type: String},
   content: {
-    links: [],
+    links: [{type: String, index: true}],
     headers: [],
     paragraphs: []
-  }
+  },
+  created_at: {type: Date, default: Date.now }
 })
 
 const Site = new Schema({
   domain: {type: String, unique: true},
   paths: [Path],
+  updated_at: {type: Date, default: Date.now, index: true },
+  created_at: {type: Date, default: Date.now, index: true },
+  blacklist: {type: Boolean, default: false}
+})
+
+Site.pre('save', function(next){
+  if(this.modified) this.updated_at = Date.now()
+
+  next()
 })
 
 module.exports = mongoose.model('Site', Site)
--- spider.js
+++ spider.js
@@ -8,21 +8,100 @@ const request = require('request'),
 
 mongoose.connect('mongodb://localhost/startup-search-engine')
 
-
-
-console.log('1')
-spider('http://www.startuptabs.com').catch( function(err){
-  console.log(err)
+async.whilst(function(){return true}, function(next){
+  spider().catch( function(err){
+    console.log(err)
+    next()
+  }).then(function(res){
+    console.log('finished', res)
+    next()
+  }, function(err){
+    console.log(err)
+    next()
+  })
 })
 
-function spider(url){
-  console.log('starting', url)
+
+function spider(){
+  return new Promise( (resolve, reject) => {
+    Site.findOne({blacklist: false}).sort({updated_at: 1}).exec( function(err, site){
+      if(site){
+        site.updated_at = Date.now()
+        site.save()
+
+        let paths_hit = []
+        async.eachSeries( site.paths, function(path, nextPath){
+          if(path.content.links.length > 0){
+            async.eachSeries(path.content.links, function(link, nextLink){
+              if(paths_hit.some( hit => hit == link)) return nextLink()
+              paths_hit.push(link)
+
+              //create url to hit
+              //check if link starts with http
+              let hit_url
+              if(link.indexOf('http') >= 0){
+                //hit the url
+                hit_url = link
+              }else{
+                //create url and then hit
+                hit_url = `http://${site.domain}${link}`
+              }
+
+              capture(hit_url).then(function(res){
+                nextLink()
+              }, function(err){
+                if(err) console.log('error hitting', hit_url, err)
+                nextLink()
+              })
+
+            },function done(){
+              nextPath()
+            })
+          }else{
+            resolve(capture(`http://${site.domain}`))
+          }
+
+        }, function done(res){
+          resolve(res)
+        })
+      }else{
+        resolve(capture('http://www.startuptabs.com/'))
+      }
+    })
+  })
+}
+
+var blacklist = false
+//capture('http://startuptabs.slack.com/ssb/download-osx')
+function capture(url){
+  console.log('capturing', url)
   return new Promise( (resolve, reject) => {
     let parsed = Url.parse(url)
     let domain = parsed.host,
       path = parsed.path
 
     async.waterfall([
+      function(done){
+        //check blacklist
+        if(blacklist){
+          let blacklisted = blacklist.some( site => site.domain == domain )
+          if(blacklisted){
+            done('blacklisted')
+          }else{
+            done()
+          }
+        }else{
+          Site.find({blacklist: true}, function(err, black){
+            blacklist = black
+            let blacklisted = black.some( site => site.domain == domain )
+            if(blacklisted){
+              done('blacklisted')
+            }else{
+              done()
+            }
+          })
+        }
+      },
       function(done){
         //check for domain entry
         Site.findOne({domain: domain}, (err, site) => {
@@ -42,11 +121,15 @@ function spider(url){
         })
       }, function(site, done){
         //check for path entry
-        let path_exists = site.paths.some( path => path == Url(url).path )
+        let path_exists = site.paths.some( path => path.path == parsed.path )
+        if(path_exists){
+          site.updated_at = Date.now()
+          site.save()
+          return done()
+        }
+        request(url, {timeout: 5000}, function(err, response, body){
+          if(err || response.statusCode != 200) return done(err)
 
-        if(path_exists) return done()
-
-        request(url, function(err, response, body){
           let $ = cheerio.load(body)
 
           let links = $('a'),
@@ -54,18 +137,19 @@ function spider(url){
             paragraphs = $('p')
 
           let entry = {
+            path: path,
             title: $('title').text(),
             description: $('meta[name=description]').attr('content'),
             content: {
-              links: links.map( (index, link) => $(link).attr('href') ),
-              headers: headers.map( (index, header) => $(header).text() ),
-              paragraphs: paragraphs.map( (index, paragraph) => $(paragraph).text() )
+              links: links.map( (index, link) => $(link).attr('href') ).get(),
+              headers: headers.map( (index, header) => $(header).text() ).get(),
+              paragraphs: paragraphs.map( (index, paragraph) => $(paragraph).text() ).get()
             }
           }
 
           site.paths.addToSet(entry)
+
           site.save( function(err, saved){
-            console.log('wtf',err);
             done(err)
           })
         })
